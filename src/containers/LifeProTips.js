d5c35a756177ff0a665a5d10d57eacdb574a7a03
osycon <fredrichawk@gmail.com>
2017-07-29 17:34:39 +0200

    Add fetching data from subreddit

--- src/containers/LifeProTips.js
+++ src/containers/LifeProTips.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
+const subreddit = `LifeProTips`;
+const url = `http://www.reddit.com/r/${subreddit}/new/.json?limit=50`;
+
 class LifeProTips extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tips: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(url).then(response => response.json()).then(data => {
+      this.setState({
+        tips: data.data.children
+      });
+      console.log(this.state.tips[3].data);
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>LifeProTips</h1>
         <article className="pro-tip">Here we will have a tip</article>
+        <ul>
+          {this.state.tips.length === 0
+            ? ``
+            : this.state.tips.map(tip =>
+                <li key={tip.data.name}>
+                  {tip.data.title}
+                </li>
+              )}
+        </ul>
       </div>
     );
   }
